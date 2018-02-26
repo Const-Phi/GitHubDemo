570d3ebe78c5dc108fba9a85dec330ac0ffbb73a
Constantine <konstantin-649@mail.ru>
2018-02-26 08:52:43 +0300

    Initial commit It's empty project for a first times.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitHubDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
